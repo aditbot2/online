66d61a96f5792156be121581725a9d1b3ac28576
aditbot2 <106464013+aditbot2@users.noreply.github.com>
2024-05-06 10:35:21 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+global.owner = ["6283133162295"]
+let fs = require('fs')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+fs.unwatchFile(file)
+console.log(`Update ${__filename}`)
+delete require.cache[file]
+require(file)
+})
